575c8feb9f45358075993352671e602a504d72ba
Simeon Naydenov <moniNaydenov@gmail.com>
2018-10-03 13:16:58 +0300

    increase allowed number of fetched events

--- calendar/classes/local/event/data_access/event_vault.php
+++ calendar/classes/local/event/data_access/event_vault.php
@@ -102,7 +102,7 @@ class event_vault implements event_vault_interface {
         $ignorehidden = true,
         callable $filter = null
     ) {
-        if ($limitnum < 1 || $limitnum > 200) {
+        if ($limitnum < 1 || $limitnum > 10000) {
             throw new limit_invalid_parameter_exception("Limit must be between 1 and 200 (inclusive)");
         }
 
